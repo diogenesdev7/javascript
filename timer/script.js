6e0999bcc6c24efd1b191e1336be8af8f9a0d9ba
Diógenes <123782296+diogenesdev7@users.noreply.github.com>
2023-04-17 17:11:45 -0300

    fiz o js

--- timer/script.js
+++ timer/script.js
@@ -1,1 +1,37 @@
+const iniciar = document.querySelector('.iniciar')
+const alarme = document.querySelector('audio')
 
+iniciar.addEventListener('click', (evt) => {
+    const hours = document.querySelector('#hours')
+    const minutes = document.querySelector('#minutes')
+    const seconds = document.querySelector('#seconds')
+
+    let duracao = (parseInt(hours.value) * 60 * 60) + (parseInt(minutes.value) * 60) + parseInt(seconds.value)
+
+    const display = document.querySelector('.display')
+    timer(duracao, display)
+
+})
+
+const timer = (duracao, display) => {
+    let timer = duracao
+    let horas, minutos, segundos
+    
+    setInterval(() => {
+        horas = Math.floor(timer / 60 / 60)
+        minutos = Math.floor(timer / 60) - (horas * 60)
+        segundos = Math.floor(timer % 60)
+
+        horas = horas < 10 ? '0' + horas : horas
+        minutos = minutos < 10 ? '0' + minutos : minutos
+        segundos = segundos < 10 ? '0' + segundos : segundos
+
+        display.innerHTML = `${horas}:${minutos}:${segundos}`
+        timer --
+
+        if (timer < 0) {
+            display.innerHTML = '00:00:00'
+            alarme.play()
+        }
+    }, 1000)
+}
